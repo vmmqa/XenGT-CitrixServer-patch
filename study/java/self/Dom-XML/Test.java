5e3573e42665f127891338b6f8454d5569241a93
Zhu Libo <libo.zhu@intel.com>
2018-01-19 04:10:07 +0800

    add package scripts
    Signed-off-by: Zhu Libo <libo.zhu@intel.com>

---
+++ study/java/self/Dom-XML/Test.java
@@ -0,0 +1,16 @@
+import xidian.sl.dom.*;
+
+public class Test{
+	public static void main(String[] args)
+	{
+
+	//读取
+        DomDemo.queryXml();
+        //插入
+        DomDemo.insertXml();
+
+
+	}
+
+
+}
